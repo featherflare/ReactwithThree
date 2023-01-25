37125fed5f21b477994ef559b25fba14eea4f785
dachatorn <dachatorn.a@kegroup.co.th>
2023-01-25 11:52:24 +0700

    Lession43 First R3F

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,19 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-import Experience from './Experience'
-import * as THREE from 'three'
+import Experience from './Experience.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-  <>
     <Canvas
-      dpr={[1, 2]}
-      gl={{
-        antialias: true,
-        toneMapping: THREE.ACESFilmicToneMapping,
-        outputEncoding: THREE.sRGBEncoding,
-      }}
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [3, 2, 6],
-      }}
+        camera={ {
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [ - 4, 3, 6 ]
+        } }
     >
-      <Experience />
+        <Experience />
     </Canvas>
-  </>
 )
