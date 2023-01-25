12af468deff32845d97f6e12263ace2e29cadac5
dachatorn <dachatorn.a@kegroup.co.th>
2023-01-25 11:47:16 +0700

    First Test R3F

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const isCodeSandbox = !!process.env.SANDBOX_URL
+
+export default {
   plugins: [react()],
-})
+  root: './',
+  publicDir: '../public/',
+  base: './',
+  server: {
+    host: true,
+    open: !isCodeSandbox, // Open if it's not a CodeSandbox
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+}
